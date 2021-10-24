6986b80e89b24c6790ec0737b28d35d9f7754567
bdavies01 <bertdaviezz@gmail.com>
2021-10-23 22:19:28 -0700

    add token storage to session

--- controllers.py
+++ controllers.py
@@ -28,16 +28,19 @@ Warning: Fixtures MUST be declared with @action.uses({fixtures}) else your app w
 from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
-from .scripts.spotifyoauth import do_oauth
+from .scripts.spotifyoauth import do_oauth, do_callback, get_token
 
 
 @unauthenticated("index", "index.html")
+@action.uses(session)
 def index():
     user = auth.get_user()
     message = T("Hello {first_name}".format(**user) if user else "Hello")
     actions = {"allowed_actions": auth.param.allowed_actions}
+    token_info, valid = get_token()
+    # print(token_info)
     return dict(message=message, actions=actions)
-	
+
 @unauthenticated("recommendations", "recommendations.html")
 def index():
     user = auth.get_user()
@@ -53,5 +56,11 @@ def index():
     return dict(message=message, actions=actions)
 
 @action("login")
+@action.uses(session)
 def login():
     return do_oauth()
+
+@action("api_callback")
+@action.uses(session)
+def api_callback():
+    return do_callback()
--- scripts/spotifyoauth.py
+++ scripts/spotifyoauth.py
@@ -3,25 +3,50 @@ spotifyoauth.py
 Functions to facilitate user login via Spotify.
 """
 import spotipy
+import time
 from spotipy.oauth2 import SpotifyOAuth
+from py4web import request, redirect
+from ..common import session
 
+client_id = "redacted"
+client_secret = "redacted"
+redirect_uri = "http://localhost:8000/spotify-open-recommendation-engine/api_callback"
+scope = "user-library-read"
+
+# User logs in and authorizes access
 def do_oauth():
+    sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
+    auth_url = sp_oauth.get_authorize_url()
+    redirect(auth_url)
+    return dict()
 
-    # Define the scope of authorization to request
-    scope = "user-library-read"
+# Request refresh/access tokens and save to session
+def do_callback():
+    session.clear()
+    sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
+    code = request.query.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info
+    redirect("index")
+    return dict()
 
-    # Grab these two fields from the Spotify Developer Dashboard
-    client_id = "nope"
-    client_secret = "nope"
+# Checks to see if token is valid and gets a new token if not
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
 
-    # The redirect URL must match the value set on the Spotify Developer Dashboard
-    redirect_uri = "http://localhost:8080"    
+    # Checking if the session already has a token stored
+    if not (session.get("token_info", False)):
+        token_valid = False
+        return token_info, token_valid
 
-    # Call Spotipy's OAuth function; bounce to the browser (redirect_uri) to login with Spotify and prompt the authorization request (scope)
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session["token_info"]["expires_at"] - now < 60
 
-    # Bonus: get & print the user's saved/liked tracks (just to prove that we could login & retrieve their data)
-    results = sp.current_user_saved_tracks()
-    for idx, item in enumerate(results['items']):
-        track = item['track']
-        print(idx, track['artists'][0]['name'], " – ", track['name'])
+    # Refresh token if expired
+    if is_token_expired:
+        sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+    token_valid = True
+    return token_info, token_valid
