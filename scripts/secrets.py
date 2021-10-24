6ccd581ec257815a75c7a99f1da7bfd837301300
Kelly Fesler <kellyfesler@gmail.com>
2021-10-24 01:29:40 -0700

    Delete placeholder secrets.py
    When this file was added to the repo, Git started tracking its changes - even though it was in the `.gitignore`. By deleting the repo's copy of the file, I'm hoping to revert that behavior.

--- scripts/secrets.py
+++
@@ -1,8 +0,0 @@
-"""
-secrets.py
-Store API credentials. Scripts read the fields from this file.
-Please do NOT commit API keys in a public place.
-"""
-
-client_id = "CLIENT_ID_FROM_SPOTIFY"
-client_secret = "CLIENT_SECRET_FROM_SPOTIFY"
