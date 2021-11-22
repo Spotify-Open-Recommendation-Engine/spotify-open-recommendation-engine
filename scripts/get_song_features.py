783c5ece75afd51d2f0f36d0455009fe69de3806
keelywhelan <43522064+keelywhelan@users.noreply.github.com>
2021-11-22 01:09:12 -0800

    Update get_song_features.py

--- scripts/get_song_features.py
+++ scripts/get_song_features.py
@@ -21,26 +21,27 @@ def get_song_features(tid):
 
         #dictionary to hold seed info of audio features
         song_features={}
-        song_features["target_danceability"] = features[0]['danceability']
-        song_features["target_energy"] = features[0]['energy']
-        song_features["target_key"] = features[0]['key']
-        song_features["target_loudness"] = features[0]['loudness']
-        song_features["target_mode"] = features[0]['mode']
-        song_features["target_speechiness"] = features[0]['speechiness']
-        song_features["target_instrumentalness"] = features[0]['instrumentalness']
-        song_features["target_valence"] = features[0]['tempo']
+        song_features["danceability"] = features[0]['danceability']
+        song_features["energy"] = features[0]['energy']
+        song_features["key"] = features[0]['key']
+        song_features["loudness"] = features[0]['loudness']
+        song_features["mode"] = features[0]['mode']
+        song_features["speechiness"] = features[0]['speechiness']
+        song_features["instrumentalness"] = features[0]['instrumentalness']
+        song_features["valence"] = features[0]['tempo']
 
         #get track info
         track_name = sp.track(tid)
 
         #set seed popularity 
         popularity = track_name['popularity']
-        song_features["target_popularity"] = popularity
+        song_features["popularity"] = popularity
 
         #get genre by getting artist (no track genre available)
         artist_uri = track_name['album']['artists'][0]['id']
         artist = sp.artist(artist_uri)
-        genres = artist['genres'] 
+        genres = artist['genres']
+        song_features["genres"] = genres
 
         #these are the valid genres seeds you are allowed to use returned from spotify endpoint 
         lg = sp.recommendation_genre_seeds()
@@ -50,7 +51,7 @@ def get_song_features(tid):
         genres_set = set(genres)
         intersection = genres_set.intersection(legal_genres)
         sgenres = list(intersection)
-        song_features["genres"] = sgenres
+        song_features["sgenres"] = sgenres
         
         #Make features dictionary into a JSON object
         song_features_json = json.dumps(song_features)
