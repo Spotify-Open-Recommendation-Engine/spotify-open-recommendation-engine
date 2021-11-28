8192e6202945c752d9c0de75f7b8926640349a2e
Claudia Stevens <claudiajstevens@gmail.com>
2021-11-28 13:20:34 -0800

    updated song info format

--- components/SongInfo.vue
+++ components/SongInfo.vue
@@ -1,43 +1,52 @@
  <template>
-    <h3>{{song}}</h3>
-    <h5>{{ artist }}</h5>
 
-    <FeatureAmount 
-      feature="Tempo" 
-      :value="tempo"
-    />
-    <FeatureAmount 
-      feature="Key" 
-      :value="key"
-    />    
-    <FeatureAmount 
-      feature="Popularity" 
-      :value="popularity"
-    />
-    <FeatureAmount 
-      feature="Acousticness" 
-      :value="acustic"
-    />
-    <FeatureAmount 
-      feature="Dancability" 
-      :value="dance"
-    />
-    <FeatureAmount 
-      feature="Energy" 
-      :value="energy"
-    />
-    <FeatureAmount 
-      feature="Instrumentalness" 
-      :value="instrumentralness"
-    />
-    <FeatureAmount 
-      feature="Speechiness" 
-      :value="speechiness"
-    />
-    <FeatureAmount 
-      feature="Valences" 
-      :value="valence"
-    />
+  <div style="border-width:thick; border:solid; border-color:blue; border-radius: 15px 50px 30px 5px">
+    <ul class="flex-container">
+    <li>
+      <container>
+        <h1>{{song}}</h1>
+        <h5 style="text-align:left">by</h5>
+        <h5>{{ artist }}</h5>
+      </container>
+    </li>  
+    <li>
+      <FeatureAmount 
+        feature="Tempo" 
+        :value="tempo"
+      />
+    </li>
+    <li>
+      <FeatureAmount 
+        feature="Key" 
+        :value="scale_key"
+      />
+    </li>
+    <li>
+      <FeatureAmount 
+        feature="Popularity" 
+        :value="popularity"
+      />
+    </li>
+    <li>
+      <FeatureAmount 
+        feature="Acousticness" 
+        :value="acustic"
+      />
+    </li>
+    <li>
+      <FeatureAmount 
+        feature="Dancability" 
+        :value="dance"
+      />
+    </li>
+    <li>
+      <FeatureAmount 
+        feature="Energy" 
+        :value="energy"
+      />
+    </li>
+  </ul>
+</div>
 </template>
 
 
@@ -50,7 +59,7 @@ export default {
     song: {required: true, type: String },
     artist: { required: true, type: String },
     tempo: { required: true, type: String },
-    key: { required: true, type: String },
+    scale_key: { required: true, type: String },
     popularity: { required: true, type: String },
     acustic: { required: true, type: String },
     dance: { required: true, type: String },
@@ -69,3 +78,27 @@ export default {
 };
 
 </script>
+
+<style>
+  .flex-container {
+  /* We first create a flex layout context */
+  display: flex;
+  
+  /* Then we define the flow direction 
+     and if we allow the items to wrap 
+   * Remember this is the same as:
+   * flex-direction: row;
+   * flex-wrap: wrap;
+   */
+  flex-flow: row wrap;
+  
+  /* Then we define how is distributed the remaining space */
+  justify-content: space-around;
+  
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+</style>
+
