37c8af16ffebbdc0291f550273a7b8855562d0a5
Kelly Fesler <kellyfesler@gmail.com>
2021-11-27 22:24:30 -0800

    Relocate search function to module with fewer dependencies

--- scripts/search.py
+++ scripts/search.py
@@ -5,10 +5,13 @@ Search Spotify for a given search_string. Returns search results.
 input: search_string
 output: search results from Spotify
 """
+import json
 import spotipy
+from py4web import response
 from .spotifyoauth import get_token
 from ..common import session
 
+# Call the Spotify API with the validated search_string
 def search_for(search_string):
 
 	# Get token from session
@@ -27,3 +30,70 @@ def search_for(search_string):
 	else:
 		return "(search_for) error: not authorized"
 
+# Validate the request and call search_for()
+def validate_and_search(request):
+
+	# Make sure some query parameter is present
+	if request.query is not None:
+
+		# Make sure query parameter q is present (and not null, empty, or only whitespace)
+		if 'q' in request.query and len(request.query.get('q')) != 0 and request.query.get('q').isspace() is False:
+				
+			# Extract the value of query parameter q
+			search_query = request.query.get('q')
+
+			# Call search_for with the query parameter
+			results = search_for(search_query)
+
+			# If the user is not authorized (session/token expired, not logged in, etc)
+			if results == "(search_for) error: not authorized":
+
+				# Return error 403: forbidden
+				response.status = 403 
+				return "(sp_search) error: not authorized"
+
+			# If the search query was null or empty (and errored out in search_for)
+			elif results == "(search_for) error: search_string null or empty":
+
+				# Return error 400: bad request
+				response.status = 400
+				return "(sp_search) error in search_for: search query (q) null or empty"
+
+			else:
+				# Otherwise, if search_for returned some response:
+				if results is not None:
+					res = []
+				
+					# Loop through the response (JSON) and extract track id, name, artist, album
+					for idx, result in enumerate(results['tracks']['items']):
+						track_id = result['id']
+						track_name = result['name']
+						track_artist = result['artists'][0]['name']
+						track_album = result['album']['name']
+						
+						# Append to an unalphabetized list (to preserve results' rank/order)
+						res.append({
+							"track_id":track_id, 
+							"track_name":track_name, 
+							"track_artist":track_artist, 
+							"track_album":track_album
+						})
+					
+					# Turn the extracted info into a JSON object and return it
+					res_json = json.dumps(res)
+					return res_json
+				
+				# If no results were found (unlikely), say so
+				else:
+					response.status = 404
+					return "(sp_search) error: no results found for search query: ", search_query
+					
+		# If query parameter was not present, was empty, or was only whitespace, return an error
+		else:
+			response.status = 400
+			return "(sp_search) error: search query (q) null or empty"
+	
+	# If we haven't returned anything by now, return an appropriate error
+	else:			
+		response.status = 400 
+		return "(sp_search) error: no query parameters found"
--- tests/test_search.py
+++ tests/test_search.py
@@ -4,30 +4,30 @@ Unit test the search.py script.
 """
 
 import json
-import spotipy
 import time
 import unittest
 from py4web import request, response
-from unittest import mock
-#from ..common import session
-from controllers import sp_search
-from scripts.search import search_for
+from unittest.mock import Mock, patch
+from scripts.search import search_for, validate_and_search
 
 class TestSearch(unittest.TestCase):
 
     # We mock the things that we need but cannot have
     def setUp():
-        mock_session = Mock()
-        mock_session["token_info"] = 'fake_info'
-        mock_session["token_info"]["expires_at"] = int(time.time()) * 2
-        mock_response = Mock()
-        mock_response.return_value = '{"message:"hello"}'
-        print(mock_session["token_info"]["expires_at"])
+        result = "not modified yet"
+        search_for("blah")
+
+    def do_mock_get_token():
+        result = "fake_token_info", True
+
+    @patch('spotifyoauth.get_token()', side_effect=do_mock_get_token())
+    def mock_get_token():
+        print(result)
 
     # TEST: search_for() with valid input
     # (search_for() is in search.py)
     # Should return: 
-    @mock.patch()
+    @patch()
     def test_search_for_valid_input():
         return True
 
@@ -46,23 +46,23 @@ class TestSearch(unittest.TestCase):
     def test_search_for_unauthorized():
         return True
 
-    # TEST: sp_search() with valid input
+    # TEST: validate_and_search() with valid input
     # (sp_search() is in controllers.py)
     # Should return: JSON w/ properties: track_id, track_name, track_artist, track_album
-    def test_sp_search_valid_input():
+    def test_validate_and_search_valid_input():
         return True
 
-    # TEST: sp_search() with bad input
+    # TEST: validate_and_search() with bad input
     # Should return: '(sp_search) error: search query (q) null or empty' (400)
-    def test_sp_search_bad_input():
+    def test_validate_and_search_bad_input():
         return True
 
-    # TEST: sp_search() with no input
+    # TEST: validate_and_search() with no input
     # Should return: '(sp_search) error: search query (q) null or empty' (400)
-    def test_sp_search_no_input():
+    def test_validate_and_search_no_input():
         return True
 
-    # TEST: sp_search(), but w/o proper auth
+    # TEST: validate_and_search(), but w/o proper auth
     # Should return: '(sp_search) error: not authorized' (403)
-    def test_sp_search_unauthorized():
+    def test_validate_and_search_unauthorized():
         return True
