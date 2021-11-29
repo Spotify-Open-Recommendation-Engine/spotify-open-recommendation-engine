4bd73c10476d6c79867dc7000fe9baa393c7d908
Kelly Fesler <kellyfesler@gmail.com>
2021-11-29 01:43:23 -0800

    Finish search_for unit tests

--- scripts/search.py
+++ scripts/search.py
@@ -1,22 +1,29 @@
 """
 search.py
 Search Spotify for a given search_string. Returns search results.
-
-input: search_string
-output: search results from Spotify
 """
 import json
 from py4web import response
 
+ERR_SEARCH_FOR_NULL = "(search_for) error: search_string null or empty"
+ERR_VALIDATE_SEARCH_NULL = "(validate_and_search) error: request is null"
+ERR_VALIDATE_SEARCH_NO_QUERY = "(validate_and_search) error: no query parameters found"
+ERR_VALIDATE_SEARCH_NO_Q = "(validate_and_search) error: query parameter 'q' not found in request"
+ERR_VALIDATE_SEARCH_BAD_Q = "(validate_and_search) error: query parameter 'q' null or empty"
+ERR_VALIDATE_SEARCH_SEARCH_FOR_ERR = "(validate_and_search) error in search_for: search query (q) null or empty"
+ERR_VALIDATE_SEARCH_NO_RESULTS = "(validate_and_search) error: no results found for search query: "
+
+# Call the Spotify API with the validated query
+# (this function is separated out for unit testing purposes)
 def call_spotipy_search(sp, query):
 	return sp.search(q=query, type='track')
 
-# Call the Spotify API with the validated search_string
+# Validate the search string and call call_spotipy_search()
 def search_for(sp, search_string):
 
 	# If search_string is null or empty, return an error
-	if search_string is None or len(search_string) == 0:
-		return "(search_for) error: search_string null or empty"
+	if search_string is None or len(search_string) == 0 or search_string.isspace():
+		return ERR_SEARCH_FOR_NULL
 		
 	else:
 		# Return 10 tracks found via search_string
@@ -25,67 +32,64 @@ def search_for(sp, search_string):
 # Validate the request and call search_for()
 def validate_and_search(sp, request):
 
-	# Make sure some query parameter is present
-	if request.query is not None:
+	# If request is None, return appropriate error
+	if request is None:
+		response.status = 400
+		return ERR_VALIDATE_SEARCH_NULL
 
-		# Make sure query parameter q is present (and not null, empty, or only whitespace)
-		if 'q' in request.query and len(request.query.get('q')) != 0 and request.query.get('q').isspace() is False:
+	# If no query parameters are present, return appropriate error
+	if request.query is None:
+		response.status = 400
+		return ERR_VALIDATE_SEARCH_NO_QUERY
+
+	# If query parameter q is not present, return appropriate error
+	if 'q' not in request.query:
+		response.status = 400
+		return ERR_VALIDATE_SEARCH_NO_Q
+
+	# If query parameter q is null, empty, or only whitespace, return appropriate error
+	if len(request.query.get('q')) == 0 or request.query.get('q').isspace() is True:
+		response.status = 400
+		return ERR_VALIDATE_SEARCH_BAD_Q
+
+	# Preliminary error-checking ok - extract the value of query parameter q
+	search_query = request.query.get('q')
+
+	# Call search_for with the query parameter
+	results = search_for(sp, search_query)
+
+	# If the search query was null or empty (and errored out in search_for)
+	if results == ERR_SEARCH_FOR_NULL:
+
+		# Return error 400: bad request
+		response.status = 400
+		return ERR_VALIDATE_SEARCH_SEARCH_FOR_ERR
+
+	else:
+		# Otherwise, if search_for returned some response:
+		if results is not None:
+			res = []
 				
-			# Extract the value of query parameter q
-			search_query = request.query.get('q')
-
-			# Call search_for with the query parameter
-			results = search_for(sp, search_query)
-
-			# If the user is not authorized (session/token expired, not logged in, etc)
-			if results == "(search_for) error: not authorized":
-
-				# Return error 403: forbidden
-				response.status = 403 
-				return "(sp_search) error: not authorized"
-
-			# If the search query was null or empty (and errored out in search_for)
-			elif results == "(search_for) error: search_string null or empty":
-
-				# Return error 400: bad request
-				response.status = 400
-				return "(sp_search) error in search_for: search query (q) null or empty"
-
-			else:
-				# Otherwise, if search_for returned some response:
-				if results is not None:
-					res = []
-				
-					# Loop through the response (JSON) and extract track id, name, artist, album
-					for idx, result in enumerate(results['tracks']['items']):
-						track_id = result['id']
-						track_name = result['name']
-						track_artist = result['artists'][0]['name']
-						track_album = result['album']['name']
+			# Loop through the response (JSON) and extract track id, name, artist, album
+			for idx, result in enumerate(results['tracks']['items']):
+				track_id = result['id']
+				track_name = result['name']
+				track_artist = result['artists'][0]['name']
+				track_album = result['album']['name']
 						
-						# Append to an unalphabetized list (to preserve results' rank/order)
-						res.append({
-							"track_id":track_id, 
-							"track_name":track_name, 
-							"track_artist":track_artist, 
-							"track_album":track_album
-						})
+				# Append to an unalphabetized list (to preserve results' rank/order)
+				res.append({
+					"track_id":track_id, 
+					"track_name":track_name, 
+					"track_artist":track_artist, 
+					"track_album":track_album
+				})
 					
-					# Turn the extracted info into a JSON object and return it
-					res_json = json.dumps(res)
-					return res_json
+			# Turn the extracted info into a JSON object and return it
+			res_json = json.dumps(res)
+			return res_json
 				
-				# If no results were found (unlikely), say so
-				else:
-					response.status = 404
-					return "(sp_search) error: no results found for search query: ", search_query
-					
-		# If query parameter was not present, was empty, or was only whitespace, return an error
+		# If no results were found (unlikely), say so
 		else:
-			response.status = 400
-			return "(sp_search) error: search query (q) null or empty"
-	
-	# If we haven't returned anything by now, return an appropriate error
-	else:			
-		response.status = 400 
-		return "(sp_search) error: no query parameters found"
+			response.status = 404
+			return ERR_VALIDATE_SEARCH_NO_RESULTS, search_query
--- tests/test_search.py
+++ tests/test_search.py
@@ -2,63 +2,97 @@
 test_search.py
 Unit test the search.py script.
 """
-
-import json
 import unittest
+from boddle import boddle
 from py4web import request, response
-from unittest.mock import Mock, MagicMock, patch
+from unittest.mock import Mock, patch
 from scripts.search import search_for, validate_and_search
+
+ERR_SEARCH_FOR_NULL = "(search_for) error: search_string null or empty"
+ERR_VALIDATE_SEARCH_NULL = "(validate_and_search) error: request is null"
+ERR_VALIDATE_SEARCH_NO_QUERY = "(validate_and_search) error: no query parameters found"
+ERR_VALIDATE_SEARCH_NO_Q = "(validate_and_search) error: query parameter 'q' not found in request"
+ERR_VALIDATE_SEARCH_BAD_Q = "(validate_and_search) error: query parameter 'q' null or empty"
+ERR_VALIDATE_SEARCH_SEARCH_FOR_ERR = "(validate_and_search) error in search_for: search query (q) null or empty"
+ERR_VALIDATE_SEARCH_NO_RESULTS = "(validate_and_search) error: no results found for search query: "
+VALID_JSON_RESPONSE = '{"message":"very cool json payload"}'
 
 class TestSearch(unittest.TestCase):
 
-    # We mock the things that we need but cannot have
- 
     # TEST: search_for() with valid input
-    # (search_for() is in search.py)
-    # Should return: 
+    # Should return: valid JSON
     @patch("scripts.search.call_spotipy_search")
-    def test_search_for_valid_input(self, mock_search_response):
+    def test_0_search_for_valid_input(self, mock_search_response):
         sp = Mock()
         q = "Sleep+Token"
-        mock_search_response.return_value = '{"message":"very cool json payload"}'
-
-        expected = '{"message":"very cool json payload"}'
+        # Our fake params for search_for() are valid - mock a valid JSON response from call_spotipy_search()
+        mock_search_response.return_value = VALID_JSON_RESPONSE
+        expected = VALID_JSON_RESPONSE
+        # search_for() calls call_spotipy_search(), which is mocked out - return the mock JSON response from above
         actual = search_for(sp, q)
         assert mock_search_response.called
         assert expected == actual
 
-    # TEST: search_for() with bad input
-    # Should return ...
-    # def test_search_for_bad_input():
-    #     return True
+    # TEST: search_for() with empty input
+    # Should return: ERR_SEARCH_FOR_NULL
+    # (no need to patch here, because call_spotipy_search() should NOT get called)
+    def test_1_search_for_empty_input(self):
+        sp = Mock()
+        q = ""
+        expected = ERR_SEARCH_FOR_NULL
+        actual = search_for(sp, q)
+        assert expected == actual
 
+    # TEST: search_for() with whitespace input
+    # Should return: ERR_SEARCH_FOR_NULL
+    # (no need to patch here, because call_spotipy_search() should NOT get called)
+    def test_2_search_for_whitespace_input(self):
+        sp = Mock()
+        q = "          "
+        expected = ERR_SEARCH_FOR_NULL
+        actual = search_for(sp, q)
+        assert expected == actual
+    
     # TEST: search_for() with no input
-    # Should return ...
-    # def test_search_for_no_input():
-    #     return True
-
-    # TEST: search_for(), but w/o proper auth
-    # Should return: '(search_for) error: not authorized'
-    # def test_search_for_unauthorized():
-    #     return True
+    # Should return: ERR_SEARCH_FOR_NULL
+    # (no need to patch here, because call_spotipy_search() should NOT get called)
+    def test_3_search_for_no_input(self):
+        sp = Mock()
+        q = None
+        expected = ERR_SEARCH_FOR_NULL
+        actual = search_for(sp, q)
+        assert expected == actual
 
     # TEST: validate_and_search() with valid input
-    # (sp_search() is in controllers.py)
     # Should return: JSON w/ properties: track_id, track_name, track_artist, track_album
-    # def test_validate_and_search_valid_input():
-    #     return True
+    # Uses boddle to 
+    # @patch("scripts.search.call_spotipy_search")
+    # def test_4_validate_and_search_valid_input(self, mock_search_response):
+    #     with boddle(query={"q":"As+Tall+As+Lions"}) as r:
+    #         sp = Mock()
+    #         #r = boddle(query={'q':'As+Tall+As+Lions'})
+    #         # r.query_string = "q=Sleep+Token"
+    #         mock_search_response.return_value = VALID_JSON_RESPONSE
+
+    #         print("DEBUG:")
+    #         print(r)
+    #         #print(r.query)
+
+    #         expected = VALID_JSON_RESPONSE
+    #         actual = validate_and_search(sp, r)
+
+    #         print(actual)
+
+    #         assert mock_search_response.called
+    #         assert expected == actual
 
     # TEST: validate_and_search() with bad input
     # Should return: '(sp_search) error: search query (q) null or empty' (400)
-    # def test_validate_and_search_bad_input():
+    #def test_validate_and_search_bad_input():
     #     return True
 
     # TEST: validate_and_search() with no input
     # Should return: '(sp_search) error: search query (q) null or empty' (400)
-    # def test_validate_and_search_no_input():
+    #def test_validate_and_search_no_input():
     #     return True
 
-    # TEST: validate_and_search(), but w/o proper auth
-    # Should return: '(sp_search) error: not authorized' (403)
-    # def test_validate_and_search_unauthorized():
-    #     return True
