72d6e44367a81a5cf5fb971d5c770c83c84fb03c
Claudia Stevens <claudiajstevens@gmail.com>
2021-11-30 12:24:46 -0800

    Changed the feature circle to represent the value of that feautre and other visual changes

--- components/FeatureInfo.vue
+++ components/FeatureInfo.vue
@@ -4,19 +4,22 @@
       class="progress-ring"
       height="120"
       width="120"
+      viewBox="-50 -50 100 100"
     >
-      <circle
-        class="progess-ring__circle"
+      <circle cx="0" cy="0" r="40" fill="none" stroke="#e6e6e6" stroke-width="10"/>
+      <path
         stroke="blue"
         stroke-width="10"
-        fill="transparent"
-        r="52"
+        fill="none"
+        r="40"
         cx="60"
         cy="60"
-      >
+        :d="path"
+        transform="rotate(180)"
+      />
       
-      </circle
-      ><text x="50%" y="50%" text-anchor="middle" stroke="#000000" stroke-width="2px" dy=".3em">{{value}}</text>
+      <text text-anchor="middle" stroke="#000000" stroke-width="1px" dy=".3em">{{value_text}}</text>
+ 
     </svg>
     
     {{feature}}
@@ -27,13 +30,62 @@
   export default {
     name: 'feature-amount',
     props: {
-      value: {required: true, type: String },
+      value_text: {required: true, type: String },
+      value: {required: true, type: String},
       feature: {required: true, type: String },
+      max: {required: false, type: String},
+    },
+    data() {
+      return {
+        val: this.value,
+        ub: this.max,
+      }
+    },
+    computed: {
+      theta(){
+        var frac;
+        if(this.value < this.max){
+          frac = this.value/this.max;
+        } else{
+          frac = (this.value-1)/this.max;
+        }
+
+        return frac * 2 * Math.PI;
+      },
+
+      path(){
+        const large = this.theta > Math.PI;
+        var arch = `M 0, -40 A 40, 40,0,${large ? 1 : 0},1,${this.endX},${this.endY}`;
+        if(this.value == this.max){
+          arch = arch +  `A 40, 40,0,0,1,0,-40`;
+        }
+        return arch;
+      },
+
+      endX(){
+        
+        return Math.cos(this.theta - Math.PI * 0.5) * 40;
+      },
+
+      endY(){
+        return Math.sin(this.theta - Math.PI * 0.5) * 40;
+      },
+
+      cssVars () {
+        return{
+          /* variables you want to pass to css */
+          '--val': this.val,
+          '--ub': this.ub,
+        }
+      },
     }
   };
 </script>
 
+
 <style>
+
+
   .d-flex {
   display: flex;
 }
@@ -45,8 +97,32 @@
 .align-items-center {
       align-items: center;
 }
+
+
 </style>
 
 
+<!-- 
+  .percent {
+  pathLength: var(--ub);
+  stroke-dasharray: var(--ub);
+  stroke-dashoffset: calc( var(--ub) - var(--val));
+}
 
 
+//in circle call at top
+        :style="cssVars"
+        class="percent"
+
+
+// in circle style
+
+ circle{
+    transform-origin: 50% 50%;
+  }
+
+
+    transition: stroke-dashoffset 0.35s;
+        transform: rotate(90deg);
+
+-->
